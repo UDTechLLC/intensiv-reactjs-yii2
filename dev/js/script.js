fbb4e5b88a1037b2bbe0b6497cdd75a1828b39e9
Miroslav <miraf1987@gmail.com>
2017-09-26 09:23:37 +0300

    add function for view school

--- dev/js/script.js
+++ dev/js/script.js
@@ -18,12 +18,12 @@ $(function(){
     $('select').on('select2:open', function(e){
         $('.select2-results__options').scrollbar().parent().addClass('scrollbar-inner');
     });
-});
 
+});
 new Vue({
     el: '#app',
     data: {
-        json: null
+        json: null,
     },
     created: function () {
         fetch("/schools.json").then(r => r.json()).then(json => {
@@ -31,3 +31,4 @@ new Vue({
         });
     }
 });
+
