1b09fb437f8c1b15a47e2868b9989961c488c0d5
Volha Alex <v.alex.lg@gmail.com>
2018-04-27 14:27:21 +0300

    change title to intensivkursstockholm.se + db settings for intensivkursstockholm

--- site/config/db.php
+++ site/config/db.php
@@ -6,11 +6,15 @@ return [
 
     'dsn' => 'mysql:host=localhost;dbname=intensivkurs', // dev
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
 
 //    'dsn' => 'mysql:host=localhost;dbname=iefsfvbw_intensivkurs', // prod
 //    'username' => 'iefsfvbw_intensivkusr',
 //    'password' => '?lIGJDzG2,m&',
+
+//    'dsn' => 'mysql:host=localhost;dbname=eamoxmoe_intensivkurs', // prod
+//    'username' => 'eamoxoe_intensivkusr',
+//    'password' => 'GnT[a4jV)kK^',
 
 
     // Schema cache options (for production environment)
